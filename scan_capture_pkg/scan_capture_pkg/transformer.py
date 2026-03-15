f883e7068cb6497def8e486f2ca9433f296920bc
Malcolm Benedict <mmbenedi@mtu.edu>
2026-03-15 17:16:35 -0400

    transformer working!

--- scan_capture_pkg/scan_capture_pkg/transformer.py
+++ scan_capture_pkg/scan_capture_pkg/transformer.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+# A simple transformer, based on: https://docs.ros.org/en/jazzy/Tutorials/Intermediate/Tf2/Writing-A-Tf2-Broadcaster-Py.html
 import math
 from geometry_msgs.msg import TransformStamped
 import numpy as np
@@ -44,24 +45,16 @@ class FramePublisher(Node):
 
         # Read message content and assign it to
         # corresponding tf variables
-        t.header.stamp = self.get_clock().now().to_msg()
+        t.header.stamp = msg.header.stamp
         t.header.frame_id = 'odom'
         t.child_frame_id = 'base_scan'
-
-        # Turtle only exists in 2D, thus we get x and y translation
-        # coordinates from the message and set the z coordinate to 0
-        t.transform.translation.x = msg.Pose.x
-        t.transform.translation.y = msg.Pose.y
-        t.transform.translation.z = 0.0
-
-        # For the same reason, turtle can only rotate around one axis
-        # and this why we set rotation in x and y to 0 and obtain
-        # rotation in z axis from the message
-        q = quaternion_from_euler(0, 0, msg.Pose.theta)
-        t.transform.rotation.x = q[0]
-        t.transform.rotation.y = q[1]
-        t.transform.rotation.z = q[2]
-        t.transform.rotation.w = q[3]
+        t.transform.translation.x = msg.pose.position.x
+        t.transform.translation.y = msg.pose.position.y
+        t.transform.translation.z = msg.pose.position.z
+        t.transform.rotation.x = msg.pose.orientation.x
+        t.transform.rotation.y = msg.pose.orientation.y
+        t.transform.rotation.z = msg.pose.orientation.z
+        t.transform.rotation.w = msg.pose.orientation.w
 
         # Send the transformation
         self.tf_broadcaster.sendTransform(t)
